beb4fbfa6cda3be7c8331802e0d033caec1eb136
Akhat09 <akhatsuleimenov@gmail.com>
2020-03-06 16:42:21 -0800

    2

--- src/fillit.c
+++ src/fillit.c
@@ -6,11 +6,18 @@
 /*   By: aksuleim <aksuleim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 13:36:30 by aksuleim          #+#    #+#             */
-/*   Updated: 2020/03/06 16:23:15 by aksuleim         ###   ########.fr       */
+/*   Updated: 2020/03/06 16:41:15 by aksuleim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "fillit.h"
+#include "libft/libft.h"
+#include <stdlib.h>
+
+void	board_cpy(char	**board, char **tmp);
+int		ft_is_free(char **board, int *address, int x, int y, int j, int board_size);
+int		fillit(t_list *lst, char **board, int board_size);
+void	free_board(char **board);
+char	**make_board(int board_size);
 
 int		get_smallest_size(t_list *lst)
 {
@@ -32,34 +39,37 @@ int		get_smallest_size(t_list *lst)
 int		fillit(t_list *lst, char **board, int board_size)
 {
 	int		i;
-	t_list	*tmp;
 	int		*address;
 	int		j;
 	int		x;
 	int		y;
 
 	i = -1;
-	address = (int *)malloc(sizeof(int) * 4);
+	address = malloc(sizeof(int) * 4);
 	if (!lst->content)
 		return (1);
 	while (++i * 4 < board_size * board_size)
 	{
 		j = 0;
+		x = 0;
+		y = 0;
 		address = (int *)(lst->content);
-		if (is_free(board, address, x, y, j))
+		if (ft_is_free(board, address, x, y, j, board_size))
 		{
 
 		}
 	}
 	// return (fillit(lst->next, board_size, board));
+	return (0);
 }
 
-int		is_free(char **board, int *address, int x, int y, int j, int board_size)
+int		ft_is_free(char **board, int *address, int x, int y, int j, int board_size)
 {
 	int		next;
 	int		curr;
 	char	**tmp;
 
+	tmp = NULL;
 	while (j < 4)
 	{
 		curr = address[j];
@@ -68,15 +78,15 @@ int		is_free(char **board, int *address, int x, int y, int j, int board_size)
 		if (board[y][x] == '.')
 		{
 			tmp[y][x] = '#';
-			return (is_free(board, address, x + next % 4 - curr % 4, y + next / 4 - curr / 4, j++, board_size));
+			return (ft_is_free(board, address, x + next % 4 - curr % 4, y + next / 4 - curr / 4, j++, board_size));
 			free_board(tmp);
 		}
 		else if (x < board_size)
-			return (is_free(board, address, x++, y, j--, board_size));
+			return (ft_is_free(board, address, x++, y, j--, board_size));
 		else if (x >= board_size && y < board_size)
 		{
 			x = 0;
-			return (is_free(board, address, x, y++, j--, board_size));
+			return (ft_is_free(board, address, x, y++, j--, board_size));
 		}
 		else
 			return (-1);
@@ -122,12 +132,12 @@ char	**make_board(int board_size)
 	str = (char **)malloc(sizeof(char *) * board_size);
 	while (++i < board_size)
 		str[i] = (char *)malloc(sizeof(char) * board_size);
-	str[i] = '\0';
+	str[i] = NULL;
 	i = -1;
 	j = 0;
 	while (++i < board_size)
 		while (j < board_size)
-			str[i][j++] == '.';
+			str[i][j++] = '.';
 	return (str);
 }
 
